ac506df699a09bbd145c247be1ab42626e4ca24c
Christian Hjelmslund <christianhjelmslund@gmail.com>
2020-07-01 14:49:36 +0200

    added a card component - will finish the main page by adding a post component by picking that issue

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React, {lazy, Suspense} from 'react';
 import {BrowserRouter, Route, Switch, Redirect} from "react-router-dom";
 
 import Layout from "./containers/Layout/Layout";
+import Card from "./components/UI/Card/Card"
 
 const App = () => {
 
     let routes = (
         <Switch>
-            <Route path={"/"} exact component={() => <p>This is the home page</p>}/>
-            <Route path={"/posts"} component={() => <p>This is the posts page</p>}/>
-            <Route path={"/investing"} component={() => <p>This is the investing page</p>}/>
+            <Route path={"/"} exact component={() => <Card>This is the home page</Card>}/>
+            <Route path={"/posts"} component={() => <Card>This is the posts page</Card>}/>
+            <Route path={"/investing"} component={() => <Card>This is the investing page</Card>}/>
             <Redirect to={"/"}/>
         </Switch>
     )
