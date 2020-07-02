3a9a8513c604ab74cb43a775c074d0769cd96f39
Christian Hjelmslund <christianhjelmslund@gmail.com>
2020-07-02 12:12:54 +0200

    added bootstrap to project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch, Redirect} from "react-router-dom";
 
+import './custom.scss'
 import Layout from "./containers/Layout/Layout";
 import Posts from "./containers/Posts/Posts"
 import Card from "./components/UI/Card/Card"
+import Button from "react-bootstrap/Button"
 
 const App = () => {
 
@@ -11,7 +13,7 @@ const App = () => {
         <Switch>
             <Route path={"/"} exact component={() => <Card>This is the home page</Card>}/>
             <Route path={"/posts"} component={Posts}/>
-            <Route path={"/investing"} component={() => <Card>This is the investing page</Card>}/>
+            <Route path={"/investing"} component={() => <Button variant="primary">Primary</Button>}/>
             <Redirect to={"/"}/>
         </Switch>
     )
--- src/containers/Posts/Post/Post.js
+++ src/containers/Posts/Post/Post.js
@@ -1,13 +1,27 @@
 import React from "react";
-import Card from "../../../components/UI/Card/Card"
+// import Card from "../../../components/UI/Card/Card"
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 
 const Post = (props) => {
     return (
-        <Card>
-            <span>title: {props.title}</span>
-            <span>author: {props.author}</span>
-            <span>{props.content}</span>
+        <Card style={{ width: '18rem' }}>
+            {/*<Card.Img variant="top" src="holder.js/100px180" />*/}
+            <Card.Body>
+                <Card.Title>{props.title}</Card.Title>
+                <p>{props.author}</p>
+                <Card.Text>
+                    {props.content}
+                </Card.Text>
+                <Button variant="primary">Read full post</Button>
+            </Card.Body>
         </Card>
+
+        // <Card>
+        //     <span>title: {props.title}</span>
+        //     <span>author: {props.author}</span>
+        //     <span>{props.content}</span>
+        // </Card>
     )
 }
 
