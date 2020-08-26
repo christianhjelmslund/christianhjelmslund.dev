15af480f395b6c1640e1f3e7e525589115ede52d
Christian Hjelmslund <christianhjelmslund@gmail.com>
2020-08-26 16:52:19 +0200

    customized and made it look way nicer, but still need to add a button to header

--- src/containers/Posts/Post/Post.js
+++ src/containers/Posts/Post/Post.js
@@ -2,18 +2,35 @@ import React from "react";
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 
+import styled from 'styled-components'
+
+const StyledCard = styled(Card)`
+        margin-bottom: 20px;
+    `
+
+const StyledButton = styled(Button)`
+    border: 2px solid white
+`
+
+const StyledText = styled.span`
+    font-style: italic
+`
+
 const Post = (props) => {
     return (
-        <Card >
+        <StyledCard bg={"dark"} text={"white"}>
+            <Card.Header as={"h5"}>
+               <StyledText>Christian Hjelmslund</StyledText>
+            </Card.Header>
             <Card.Body>
                 <Card.Title>{props.title}</Card.Title>
                 <p>{props.author}</p>
                 <Card.Text>
                     {props.content}
                 </Card.Text>
-                <Button variant="primary">Read full post</Button>
+                <StyledButton variant={"dark"}>Read full post</StyledButton>
             </Card.Body>
-        </Card>
+        </StyledCard>
     )
 }
 
